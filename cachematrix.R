87d1339d18d88ab84cdd9b207499890d11c46580
Barnaba <barnaba@umcs.pl>
2014-08-23 10:33:42 +0200

    Updated assignment task

--- cachematrix.R
+++ cachematrix.R
@@ -5,12 +5,11 @@
 ## If so, returns stored inverted matrix (y), otherwise make new computation.
 
 
-## First function contains 3 other functions:
+## First function contains 4 other functions:
+## set - assigns new matrix as a given matrix
 ## get - returns the given matrix
 ## getinverse - returns the inverted matrix
 ## setinverse - cache the inverted matrix
-## I haven't included "set" function, because it is
-## redundant for assignment requirements
 ## I named 'setinverse' argument as 'z' because
 ## it is only an argument, not a predefined function
 ## name. If you use predefined function name from
@@ -20,21 +19,27 @@
 
 makeCacheMatrix <- function (x = matrix()){
       y <- NULL
+      set <- function(v) {
+            x <<- v
+            y <<- NULL
+      }
       get <- function() x
       setinverse <- function(z) y <<- z
       getinverse <- function() y
-      list (get=get, setinverse=setinverse, getinverse = getinverse)
+      list (set=set, get=get, setinverse=setinverse, getinverse = getinverse)
 }
 
 ## Second function cumputes matrix inversion using solve function,
 ## but firstly check if it is cache matrix for 'x' value stored
-## within makeCacheMatrix. If it is, the function just returns
-## the inverted cached matrix
+## within makeCacheMatrix (using 'getinverse' subfunction. If it 
+## is, the function just returns the inverted cached matrix. Otherwise,
+## 'solve' function is executed on a given matrix. The computation's
+## result is stored within previous function (using 'setinverse' subfuntion). 
 
 cacheSolve <- function(x, ...){
       y <- x$getinverse()
       if (!is.null(y)) {
-            message ("Getting cached matrix")
+            message ("Getting cached matrix...")
             return (y)
       }
       dane <- x$get()
